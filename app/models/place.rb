ba105db1f75e4177f7152dfac87405d628c4789a
ukroma1992 <ukroma1992@gmail.com>
2017-05-15 21:14:36 +0300

    Added average score calculation

--- app/models/place.rb
+++ app/models/place.rb
@@ -10,4 +10,10 @@ class Place < ActiveRecord::Base
   after_validation :geocode
 
   has_many :reviews, dependent: :destroy
+
+  def average_rating
+    self.reviews.sum(:score) / reviews.size
+  rescue ZeroDivisionError
+    0
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170515142451) do
+ActiveRecord::Schema.define(version: 20170515175228) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,10 +23,11 @@ ActiveRecord::Schema.define(version: 20170515142451) do
     t.string   "phone"
     t.string   "website"
     t.integer  "user_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
     t.float    "latitude"
     t.float    "longitude"
+    t.integer  "total_average_rating", default: 0
   end
 
   create_table "reviews", force: :cascade do |t|

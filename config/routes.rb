005e82d9181fa2e01728c4ae27ad9afb527093a0
ukroma1992 <ukroma1992@gmail.com>
2017-05-15 22:19:26 +0300

    Created a dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+
   resources :places
   resources :reviews
+
   devise_for :users, controllers: {registrations: "registrations"}
+
   root 'pages#home'
+  get 'dashboard', to: 'pages#dashboard'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

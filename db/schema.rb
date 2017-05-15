92791393e8fa84716df538fced2aed2dd3a76198
ukroma1992 <ukroma1992@gmail.com>
2017-05-15 18:33:53 +0300

    Added scores to reviews using raty

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170515113641) do
+ActiveRecord::Schema.define(version: 20170515142451) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,8 +33,9 @@ ActiveRecord::Schema.define(version: 20170515113641) do
     t.integer  "place_id"
     t.integer  "user_id"
     t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.integer  "score",      default: 0
   end
 
   create_table "users", force: :cascade do |t|
